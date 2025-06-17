9db99b063aa37944165052e57d0a7b5e6aeca1b2
qingcanglingze <chsv@foxmail.com>
2025-06-17 18:17:16 +0800

    Site updated: 2025-06-17 18:17:16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1750154458893";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1750155431740";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
